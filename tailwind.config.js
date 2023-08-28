6ba5e30a022bedf17b522470dab53c44108455b3
gmastro <george.mastrovasilis@gmail.com>
2023-08-28 03:36:14 +0300

    extending tailwind preprocessed name conventions, calculations and vars. - added json files support

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-text)",
+        secondary: "var(--color-heading)",
+        "bg-primary": "var(--color-background)",
+        "bg-soft": "var(--color-background-soft)",
+        "bg-mute": "var(--color-background-mute)",
+        "border-primary": "var(--color-border)",
+        "border-secondary": "var(--color-border-hover)",
+      },
+      spacing: {
+        "divider": "var(--section-gap)",
+        "divider-2": "calc(var(--section-gap) / 2)",
+        "shift-25": "calc(50% + 25px)",
+        "unshift-25": "calc(50% - 25px)"
+      }
+    },
   },
   plugins: []
 }
