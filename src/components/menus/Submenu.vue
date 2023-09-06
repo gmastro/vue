2bcf08d68de4f4bdd287f5505f24b97a563b2b53
gmastro <george.mastrovasilis@gmail.com>
2023-09-07 01:26:08 +0300

    commit before remove

--- src/components/menus/Submenu.vue
+++ src/components/menus/Submenu.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
-import { type Route } from '@/functions/helpers';
+import { type Route, toggleDropdown, toggleSubmenu } from '@/functions/helpers';
+import IconDropdownArrow from '../icons/IconDropdownArrow.vue';
 
 defineProps<{
   routes: Route[] | undefined,
   toShow: boolean
 }>()
+
+let menu: {[key: string]: boolean} = {};
 
 </script>
 
@@ -12,6 +15,12 @@ defineProps<{
     <ul :class="[toShow ? 'block' : 'hidden']">
         <li v-for="(route, index) in routes" :key="route.name">
             <RouterLink :to="route.href">{{ route.text }}</RouterLink>
+            <div v-if="route.hasChildren"
+                @click="toggleDropdown(route.name, menu)"
+                 class="dropdown">
+                <IconDropdownArrow />
+                <Submenu :toShow="toggleSubmenu(route.name, menu)" :routes="route.children" />
+            </div>
         </li>
     </ul>
 </template>
